42a872c29e4989964ec683498f9684f5eb1736e1
¨Carlos <carlosjtapia12798@hotmail.com>
2025-05-27 17:05:57 -0400

    Primer commit

---
+++ FerreteriaElClavo/Data/Database.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace FerreteriaElClavo.Data
+{
+    public static class Database
+    {
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["FerreteriaDB"].ConnectionString);
+        }
+    }
+}
